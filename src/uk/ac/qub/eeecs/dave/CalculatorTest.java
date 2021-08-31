cb66498fba85200b3cbf00ac5ac530fba074a6c3
Michael Kielt <mkielt01@qub.ac.uk>
2021-08-31 18:21:36 +0100

    test failures

--- src/uk/ac/qub/eeecs/dave/CalculatorTest.java
+++ src/uk/ac/qub/eeecs/dave/CalculatorTest.java
@@ -9,18 +9,18 @@ class CalculatorTest {
 	@Test
 	void testAdd() {
 		Calculator c = new Calculator();
-		assertEquals(c.add(10,1), 11);
+		assertEquals(c.add(10,2), 11);
 	}
 	
 	@Test
 	void testAdd2() {
 		Calculator c = new Calculator();
-		assertEquals(c.add(15,1), 16);
+		assertEquals(c.add(15,2), 16);
 	}
 	
 	@Test
 	void testSub() {
 		Calculator c = new Calculator();
-		assertEquals(c.sub(10,1), 9);
+		assertEquals(c.sub(10,2), 9);
 	}
 }
